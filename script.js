696589f4fed47a29c1ee5553decd9e0342458209
Balaji S <104999488+Typical-Difference@users.noreply.github.com>
2025-03-25 22:18:32 +0530

    Improvements to Game(), addition of gameController()

--- script.js
+++ script.js
@@ -1,33 +1,57 @@
+//Game is a factory function
+//Single instance -> module pattern
 function Game() {
-    let gameBoard = [['','','',], ['','','',], ['','','',]]; //3 x 3 tile private variable
+    let board = [['','','',], ['','','',], ['','','',]]; //3 x 3 tile private variable
+
+    function getBoard(row, col){
+        return board[row][col];
+    }
+
+    function setBoard(row, col, tileInput){
+        board[row][col] = tileInput;
+    }
+
+    function modifyTile(row,col,tileInput){
+        if(board[row][col] === ''){
+            board[row][col] = tileInput;
+        }
+        else{
+            //User inputs into a tile that is already filled --> Ignore
+        }
+    }
+
+    function display(){
+        return board;
+    }
+
+    function resetBoard(){
+        board = [['','','',], ['','','',], ['','','',]];
+    }
+
+    function checkWinner(){
+        const winningCombinations = [
+            [[0,0], [0,1], [0,2]],
+            [[1,0], [1,1], [1,2]],
+            [[2,0], [2,1], [2,2]],
+            [[0,0], [1,0], [2,0]],
+            [[0,1], [1,1], [2,1]],
+            [[0,2], [1,2], [2,2]],
+            [[0,0], [1,1], [2,2]],
+            [[0,2], [1,1], [2,0]]
+        ];
+        for(const combination of winningCombinations){
+            const[a, b, c] = combination;
+            if(board[a[0]][a[1]] === board[b[0]][b[1]] && 
+                board[a[0]][a[1]] === board[c[0]][c[1]] && 
+                board[b[0]][b[1]] === board[c[0]][c[1]]){
+                    return board[a[0]][a[1]];
+            }
+        }
+    }
 
     return {
         //return functions that perform operations on the gameBoard
-        getBoard(row, col){
-            return gameBoard[row][col];
-        },
-
-        setBoard(row, col, tileInput){
-            gameBoard[row][col] = tileInput;
-        },
-
-        modifyTile(row,col,tileInput){
-            if(gameBoard[row][col] === ''){
-                gameBoard[row][col] = tileInput;
-            }
-            else{
-                //User inputs into a tile that is already filled --> Ignore
-            }
-        },
-
-        display(){
-            return gameBoard;
-        },
-
-        resetBoard(){
-            gameBoard = [['','','',], ['','','',], ['','','',]];
-        }
-
+        getBoard, setBoard, modifyTile, display, resetBoard, checkWinner
     };
 
 }
@@ -37,18 +61,32 @@ function Player(name, tileInput){
 }
 
 const tic = Player('Tic', 'X');
-const tac = Player('Tac', 'Y');
+const tac = Player('Tac', 'O');
 
-function GameController(p1, p2){
+//Single instance -> module pattern
+function GameController (p1, p2){
+    currentPlayer = p1;
     let scoreP1 = 0;
     let scoreP2 = 0;
 
-    return {
-        increment(score){
-            score++;
-        },
-        display(){
-            return {scoreP1, scoreP2};
+    function switchTurn(){
+        currentPlayer = (currentPlayer === p2) ? p1 : p2;
+    }
+
+    function playTurn(row, col){
+        if(Game.getBoard(row, col) === ''){
+            setBoard(row, col, currentPlayer.tileInput);
         }
     }
+
+    function increment(score){
+        return score++;
+    }
+
+    function display(){
+        return {scoreP1, scoreP2};
+    }
+
+    return {increment, display};
 }
+
